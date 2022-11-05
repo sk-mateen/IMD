494ae78be96788c632485f54556e5e401b81ce02
Razaaq <rajaak1589@gmail.com>
2022-11-05 19:01:30 +0530

    make it be better

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,7 +1,7 @@
 /* GET homepage */
-const index = (req, res) => {
+const about = (req, res) => {
     res.render('index', { title: 'IMD food services' });
    };
    module.exports = {
-    index
+    about
 };
